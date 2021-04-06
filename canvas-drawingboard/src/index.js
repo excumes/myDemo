d4b5fa456e0e40d5e984f7c423685dab01ae9b37
excumes <627261443@qq.com>
2021-04-06 18:57:04 +0800

    增加配置文件

--- canvas-drawingboard/src/index.js
+++ canvas-drawingboard/src/index.js
@@ -1,8 +1,51 @@
-function Print() {
-    console.log(this.loginId);
+import "./assets/main.less"
+
+
+const canvas = document.getElementById("board");
+const ctx = canvas.getContext("2d");
+let painting = false;
+let lastPoint = { x: null, y: null };
+
+// 设置画布大小
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+
+canvas.onmousedown = (e) => {
+    painting = true;
+    const x = e.clientX;
+    const y = e.clientY;
+    lastPoint = { x, y };
+    drawCircle(x, y, 5);
+}
+canvas.onmousemove = (e) => {
+    if(painting){
+        const x = e.clientX;
+        const y = e.clientY;
+        let newPoint = { "x": x, "y": y};
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y);
+        lastPoint = newPoint;
+    }
+}
+canvas.onmouseup = () => {
+    painting = false;
 }
 
-const obj = {
-    loginId: "abc"
-};
-Print.call(obj);
+//画点
+function drawCircle(x, y, radius) {
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fill();
+}
+
+
+// 划线函数
+function drawLine(x1, y1, x2, y2) {
+    ctx.lineWidth = 3;
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+}
--- canvas-drawingboard/webpack.config.js
+++ canvas-drawingboard/webpack.config.js
@@ -1,18 +1,29 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const path = require("path");
 
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
+        path: path.resolve(__dirname, "./dist"),
         filename: "[id].[chunkhash:5].js",
     },
     devtool: "eval-source-map",
+    devServer: {
+        port: 8000,
+        open: true,
+    },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: "./public/index.html"
         }),
-    ]
+    ],
+    module: {
+        rules: [
+            {test: /\.js$/, use: "babel-loader"},
+            {test: /\.less$/, use: ["style-loader", "css-loader", "less-loader"],}
+        ]
+    }
 }
